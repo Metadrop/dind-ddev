35afbaabe510d41f6288b74fe5a70d31ce230105
Jonathan Araña <jonathan.arana@metadrop.net>
2025-02-28 19:18:28 +0100

    Resize filesystem instead of copy over

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+truncate -s $BTRFS_SIZE_DEFAULT $BTRFS_FILE_DEFAULT
 btrfs-mount.sh
+btrfs filesystem resize max $DATA_ROOT_DEFAULT
 
-dockerd --tls=false --host=unix:///var/run/docker.sock,tcp://0.0.0.0:2375
+dockerd --tls=false --host=tcp://0.0.0.0:2375
