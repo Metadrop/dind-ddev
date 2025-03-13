155e573a326f7a9651279fa7f6a87b5d56bf7e36
Jonathan Araña Cruz <jonhattan@faita.net>
2025-03-13 23:09:06 +0100

    dockerd - bind also to unix socket

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+echo "Resizing images volume to ${BTRFS_SIZE_DEFAULT}..."
 truncate -s $BTRFS_SIZE_DEFAULT $BTRFS_FILE_DEFAULT
 /usr/local/bin/btrfs-mount.sh
 btrfs filesystem resize max $DATA_ROOT_DEFAULT
 
-dockerd --tls=false --host=tcp://0.0.0.0:2375
+dockerd --tls=false --host=tcp://0.0.0.0:2375 --host=unix:///var/run/docker.sock
